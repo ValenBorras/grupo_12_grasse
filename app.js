9620d7c5ae50e4599792414e89d819e521363580
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2022-07-22 11:21:58 -0300

    estructura express y archivos html y css

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const path= require('path');
+const port= process.env.port || 3030; 
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.listen(port, () => console.log('Servidor corriendo...'));
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname,'./views/index.html'))
+});
