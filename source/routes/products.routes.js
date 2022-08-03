ff8961fb19ea13f64c293c12f629b3940b0bb250
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2022-08-03 19:44:40 -0300

    Eliminacion controller y router

--- source/routes/products.routes.js
+++ source/routes/products.routes.js
@@ -1,8 +1,5 @@
 const {Router} = require("express")
 const route = Router()
-const controller = require("../controllers/products.controller")
 
-route.get('/productos/:categoria?',controller.index)
-route.get('/productos/detalle/:producto',controller.show)
 
 module.exports = route;
