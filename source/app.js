bad18b40e048f0140d845a39e2e97d0822781219
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2022-08-03 22:32:31 -0300

    instalacion ejs, nodemon y aplicacion de vistas parciales

--- source/app.js
+++ source/app.js
@@ -1,34 +1,36 @@
 const express = require('express');
 const server = express();
 const config = require('./modules/server');
-server.listen(config.port,config.start());
 
+server.listen(config.port,config.start());
 const {join} = require('path');
+
+server.set('views', join(__dirname,'./views'));
+server.set('view engine', 'ejs');
+
 const statics = require('./modules/static');
 server.use(statics(join(__dirname,"../public")));
 
-const productsRoutes = require('./routes/products.routes'); 
-server.use(productsRoutes); 
-
+//server.use(require('./routes/products.routes.js')); 
 
 server.get('/', (req,res)=> {
-    res.sendFile(join(__dirname,'./views/index.html'))
+    res.render(join(__dirname,'./views/index.ejs'))
 });
 
 server.get('/detalle', (req,res)=> {
-    res.sendFile(join(__dirname,'./views/detail.html'))
+    res.render(join(__dirname,'./views/detail.ejs'))
 });
 
 server.get('/login', (req,res)=> {
-    res.sendFile(join(__dirname,'./views/login.html'))
+    res.render(join(__dirname,'./views/login.ejs'))
 });
 
 server.get('/register', (req,res)=> {
-    res.sendFile(join(__dirname,'./views/register.html'))
+    res.render(join(__dirname,'./views/register.ejs'))
 });
 
 server.get('/carrito', (req,res)=> {
-    res.sendFile(join(__dirname,'./views/cart.html'))
+    res.render(join(__dirname,'./views/cart.ejs'))
 });
 
 
--- source/routes/products.routes.js
+++ source/routes/products.routes.js
@@ -1,5 +1,25 @@
-const {Router} = require("express")
-const route = Router()
+/*const {Router} = require("express")
+const router = Router()
+const {join} = require('path');
 
+router.get('/', (req,res)=> {
+    res.render(join(__dirname,'./views/index.ejs'))
+});
 
-module.exports = route;
+router.get('/detalle', (req,res)=> {
+    res.render(join(__dirname,'./views/detail.ejs'))
+});
+
+router.get('/login', (req,res)=> {
+    res.render(join(__dirname,'./views/login.ejs'))
+});
+
+router.get('/register', (req,res)=> {
+    res.render(join(__dirname,'./views/register.ejs'))
+});
+
+router.get('/carrito', (req,res)=> {
+    res.render(join(__dirname,'./views/cart.ejs'))
+});
+
+module.exports = router;*/
