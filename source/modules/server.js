8c917e21daa198965b149ab3ca6d4818055aeac9
DanCurzo <benitez.e.joaquin@gmail.com>
2022-08-11 20:27:30 -0300

    actualizacion: en archivo server.js

--- source/modules/server.js
+++ source/modules/server.js
@@ -1,3 +1,3 @@
 const port = process.env.PORT || 3030;
-const start = () => console.log('Servidor Corriendo...');
+const start = () => console.log(`Servidor corriendo en puerto ${port}`);
 module.exports = {port,start};
