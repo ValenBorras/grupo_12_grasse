626102cd0b6eab02d806542e69b63b1c8978bf7f
emolinaf99 <molii.esteban@gmail.com>
2022-08-17 17:08:41 -0500

    creación de sección Testimonios, pendientes pequeños ajustes responsive

--- source/controllers/index.controller.js
+++ source/controllers/index.controller.js
@@ -1,6 +1,9 @@
 const controller  = {
     index: (req,res)=> {
         res.render('index')
+    },
+    testimonios: (req,res)=> {
+        res.render("testimonios")
     }
 }
 
--- source/routes/index.routes.js
+++ source/routes/index.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 const controller = require('../controllers/index.controller')
 
 router.get('/', controller.index);
-
+router.get("/Testimonios", controller.testimonios);
 
 
 module.exports = router;
