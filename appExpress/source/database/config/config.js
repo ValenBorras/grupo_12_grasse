f6d3b497d92e8c313e5238a05f571d32fcdc3760
ValenBorras <106563001+ValenBorras@users.noreply.github.com>
2022-12-07 12:12:35 -0300

    config para deploy de db

--- appExpress/source/database/config/config.js
+++ appExpress/source/database/config/config.js
@@ -1,23 +1,26 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "grasse_db",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER || "root",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_NAME || "grasse_db",
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "port": process.env.DB_PORT || "3306",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER ||"root",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_NAME || "database_test",
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "port": process.env.DB_PORT || "3306",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER || "root",
+    "password": process.env.DB_PASSWORD || null,
+    "database": process.env.DB_NAME || "database_production",
+    "host": process.env.DB_HOST || "127.0.0.1",
+    "port": process.env.DB_PORT || "3306",
     "dialect": "mysql"
   }
 }
